4952a64c0d689f2d8ea9e3f41cc2d97f512a2149
YaNeShizoid <danyshilov5@gmail.com>
2023-07-11 21:58:27 +0300

    make todo app and model

---
+++ backend/todo/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.conf import settings
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
